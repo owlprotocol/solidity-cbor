0ecf8be9e4253faf72e6b4eb12a9224a0b81e2e1
Corban Villa <corbanvilla@gmail.com>
2022-04-10 13:34:31 +0400

    file refactoring

--- test/CBORDecoding.ts
+++ test/CBORDecoding.ts
@@ -1,7 +1,7 @@
 import { assert, expect } from "chai";
 import { ethers } from "hardhat";
 import cbor from "cbor";
-import { toHex, padLeft, utf8ToHex } from "web3-utils";
+import { toHex } from "web3-utils";
 import {
     // eslint-disable-next-line camelcase
     CBORDecoding__factory,
@@ -12,6 +12,7 @@ import {
 } from "../typechain";
 // eslint-disable-next-line node/no-missing-import
 import dndData from "./sampleDatasets/dndData";
+import { toExpectedValue, toProperHex } from "./testUtils";
 import { ContractFactory } from "ethers/lib/ethers";
 
 describe("CBOR Decoding", function () {
@@ -230,57 +231,3 @@ describe("CBOR Decoding", function () {
     });
 });
 
-export function toExpectedValue(value: any) {
-    /**
-     * Testing is complicated by the fact that CBORDecode is nest-aware, but
-     * cannot decode and return a nested object. Instead, it has to decode one
-     * layer at a time (due to polymorphism limitations of Solidity).
-     */
-    let expectedValue;
-    if (Array.isArray(value)) expectedValue = value.map((v) => toProperHex(v));
-    else if (typeof value === "object")
-        expectedValue = Object.entries(value).map(([k, v]) => [
-            utf8ToHex(k),
-            toProperHex(v),
-        ]);
-    else expectedValue = toProperHex(value);
-
-    return expectedValue;
-}
-
-export const toProperHex = (v: any) => {
-    let vencoded;
-    if (typeof v === "object")
-        // Convert nested objects to cbor
-        vencoded = "0x" + cbor.encode(v).toString("hex");
-    else if (typeof v === "number") {
-        // Convert to hex and pad appropriately
-        vencoded = base2Padding(toHex(v));
-    } else if (typeof v === "string") {
-        // Strings don't need special padding
-        vencoded = utf8ToHex(v);
-    } else {
-        vencoded = toHex(v);
-    }
-    return vencoded;
-};
-
-export const base2Padding = (hexString: string) => {
-    /**
-     * Calculates and formats proper byte padding for expected Solidity bytes.
-     * A hex with 3 digits (0x003) needs 4 digits of padding (0x0004).
-     * A hex with 5 digits needs 8 digits of padding.
-     * A hex with 12 digits needs 16 digits of padding.
-     * It's always the next highest base2 value. (2,4,8,16,32,...)
-     */
-    // Slice off '0x'
-    let hexLength = hexString.length;
-    if (hexString.slice(0, 2) === "0x") hexLength -= 2;
-    // Calculate closest base2 exponent (2, 4, 8, etc)
-    let base2Exponent;
-    if (hexLength > 1) {
-        base2Exponent = Math.ceil(Math.log2(hexLength));
-    } else base2Exponent = 1;
-    // Return padded
-    return padLeft(hexString, 2 ** base2Exponent);
-};
---
+++ test/testUtils.ts
@@ -0,0 +1,57 @@
+import { toHex, padLeft, utf8ToHex } from "web3-utils";
+import cbor from "cbor";
+
+export function toExpectedValue(value: any) {
+    /**
+     * Testing is complicated by the fact that CBORDecode is nest-aware, but
+     * cannot decode and return a nested object. Instead, it has to decode one
+     * layer at a time (due to polymorphism limitations of Solidity).
+     */
+    let expectedValue;
+    if (Array.isArray(value)) expectedValue = value.map((v) => toProperHex(v));
+    else if (typeof value === "object")
+        expectedValue = Object.entries(value).map(([k, v]) => [
+            utf8ToHex(k),
+            toProperHex(v),
+        ]);
+    else expectedValue = toProperHex(value);
+
+    return expectedValue;
+}
+
+export const toProperHex = (v: any) => {
+    let vencoded;
+    if (typeof v === "object")
+        // Convert nested objects to cbor
+        vencoded = "0x" + cbor.encode(v).toString("hex");
+    else if (typeof v === "number") {
+        // Convert to hex and pad appropriately
+        vencoded = base2Padding(toHex(v));
+    } else if (typeof v === "string") {
+        // Strings don't need special padding
+        vencoded = utf8ToHex(v);
+    } else {
+        vencoded = toHex(v);
+    }
+    return vencoded;
+};
+
+export const base2Padding = (hexString: string) => {
+    /**
+     * Calculates and formats proper byte padding for expected Solidity bytes.
+     * A hex with 3 digits (0x003) needs 4 digits of padding (0x0004).
+     * A hex with 5 digits needs 8 digits of padding.
+     * A hex with 12 digits needs 16 digits of padding.
+     * It's always the next highest base2 value. (2,4,8,16,32,...)
+     */
+    // Slice off '0x'
+    let hexLength = hexString.length;
+    if (hexString.slice(0, 2) === "0x") hexLength -= 2;
+    // Calculate closest base2 exponent (2, 4, 8, etc)
+    let base2Exponent;
+    if (hexLength > 1) {
+        base2Exponent = Math.ceil(Math.log2(hexLength));
+    } else base2Exponent = 1;
+    // Return padded
+    return padLeft(hexString, 2 ** base2Exponent);
+};
